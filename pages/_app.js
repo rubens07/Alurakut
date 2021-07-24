c3b26ba75b064b359b2cc1b34d178be2dbb45a61
rubens <rubens.c17@gmail.com>
2021-07-24 15:45:45 -0300

    Desenvolvimento da 1ª aula

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from 'src/lib/AlurakutUtils'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+
+  body {
+    background-color: #d4dded;
+    font-family: sans-serif;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,66 @@
-import styled from 'styled-components'
+import MainGrid from '@/MainGrid';
+import Box from '@/Box';
+import {
+  AlurakutMenu, OrkutNostalgicIconSet
+} from 'src/lib/AlurakutUtils';
+import ProfileRelationsBoxWrapper from '@/ProfileRelations';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+function ProfileSidebar({ user }) {
+  return (
+    <Box>
+      <img src={`/${user}`} style={{ borderRadius: "8px" }}/>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const user = "profileavatar.png";
+  const amigos = [
+    'imagem1.jpeg', 'imagem2.jpg',
+    'imagem3.jpg', 'imagem4.jpg',
+    'imagem5.jpg', 'imagem6.jpg',
+  ];
+
+  return (
+    <>
+      <AlurakutMenu/>
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar user={user} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            <h1 className="title">
+              Bem Vindo(a)
+            </h1>
+
+            <OrkutNostalgicIconSet/>
+          </Box>
+        </div>
+        <div className="relationsArea" style={{ gridArea: "relationsArea" }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Amigos ({amigos.length})
+            </h2>
+            <ul>
+              {amigos.map((item, index) => {
+                return (
+                  <li>
+                    <a href="#" key={index}>
+                      <img src={`/${item}`}/>
+                      <span>{item}</span>
+                    </a>
+                  </li>
+                )})
+              }
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>
+            Comunidades
+          </Box>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
